f63dff4f962f1d4cb61db3cbfea1acd0a352d4f0
ivanvivasga <114440414+ivanvivasga@users.noreply.github.com>
2023-05-31 15:43:21 -0600

    ACTUALIZACION PROYECTO FINAL

---
+++ Juego Gato/Juego Gato/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Juego_Gato
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fireAndBlood = "/FireAndBlood.png";
+        string hearMeRoar = "/LannisterSigil.jpg";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0x0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn0x1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn0x2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1x0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1x1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1x2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2x0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2x1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2x2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInicio_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
